21560b4c39a736c6b363f7e8ccd434e7abf27da0
Kirian Murgadella <844565+KirianM@users.noreply.github.com>
2022-12-27 14:27:45 +0000

    Update Corne layout

--- keyboards/splitkb/aurora/corne/keymaps/kirianm/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/kirianm/keymap.c
@@ -11,33 +11,33 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[L_BASE] = LAYOUT_split_3x6_3(
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                           KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RALT,
-        KC_LGUI, KC_A, KC_S, KC_D, KC_F, KC_G,                          KC_H, KC_J, KC_K, KC_L, LT(L_ARROWS, KC_SCLN), KC_QUOT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                          KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESC,
-        KC_LCTL, KC_SPC, MO(L_NUMBERS),                                 MO(L_SYMBOLS), KC_ENT, KC_BSPC
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                          KC_H, KC_J, KC_K, KC_L, LT(L_ARROWS, KC_SCLN), KC_QUOT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                          KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ESC),
+        KC_LGUI, KC_SPC, MO(L_NUMBERS),                                 MO(L_SYMBOLS), KC_ENT, KC_BSPC
     ),
 	[L_NUMBERS] = LAYOUT_split_3x6_3(
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, KC_1, KC_2, KC_3, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, KC_1, KC_2, KC_3, XXXXXXX, _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, KC_4, KC_5, KC_6, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, KC_7, KC_8, KC_9, XXXXXXX, XXXXXXX,
         _______, _______, XXXXXXX,                                      MO(L_SETTINGS), KC_0, _______
     ),
 	[L_ARROWS] = LAYOUT_split_3x6_3(
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, _______, XXXXXXX,                                      XXXXXXX, _______, _______
+        _______, _______, XXXXXXX,                                      XXXXXXX, _______, KC_DEL
     ),
 	[L_SYMBOLS] = LAYOUT_split_3x6_3(
         _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,              KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
-        _______, _______, XXXXXXX,                                      XXXXXXX, _______, _______
+        _______, _______, MO(L_FN),                                     XXXXXXX, _______, _______
     ),
 	[L_FN] = LAYOUT_split_3x6_3(
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F10, _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, KC_F4, KC_F5, KC_F6, KC_F11, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F12, XXXXXXX,
-        _______, _______, MO(L_FN),                                     XXXXXXX, _______, _______
+        _______, _______, XXXXXXX,                                      XXXXXXX, _______, _______
     ),
 	[L_SETTINGS] = LAYOUT_split_3x6_3(
         QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
